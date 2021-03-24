fa8b59760909f92171ac62528e0c0710ec07aa1c
unknown <ms.anna.kobzar@gmail.com>
2021-03-25 00:00:47 +0300

    commit24/03-2

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,27 +1,27 @@
 import React from "react";
 import TodoListItem from "../todo-list-item/todo-list-item";
 
-import './todo-list.css'
+import "./todo-list.css";
 
-const TodoList = ({ todos, onDeleted }) => {
+const TodoList = ({ todos, onDeleted, onToggleImportant, onToggleDone }) => {
   const elements = todos.map((item) => {
     const { id, ...itemProps } = item;
-    
+
     return (
-      <li key={ id} className="list-group-item">
+      <li key={id} className="list-group-item">
         {/* <TodoListItem label={item.label} important={item.important} /> */}
         {/* also: */}
-        <TodoListItem { ...itemProps} 
-        onDeleted = {() => onDeleted(id)} />
+        <TodoListItem
+          {...itemProps}
+          onDeleted={() => onDeleted(id)}
+          onToggleImportant={() => onToggleImportant(id)}
+          onToggleDone={() => onToggleDone(id)}
+        />
       </li>
     );
   });
 
-  return (
-    <ul className="list-group todo-list">
-      { elements }
-    </ul>
-  );
+  return <ul className="list-group todo-list">{elements}</ul>;
 };
 
 export default TodoList;
